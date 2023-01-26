afcd7bd19c1f9a1bdcb0b9a344c5356fb7913ae6
Πάτροκλος Γεωργίαδης <efthimisgeorgiadis17@gmail.com>
2023-01-27 01:12:00 +0200

    starting the login process

--- app/src/main/java/com/example/smartalert/MainActivity.java
+++ app/src/main/java/com/example/smartalert/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.smartalert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText email,password,data;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email= findViewById(R.id.editTextTextPersonName2);
+        password=findViewById(R.id.editTextTextPersonName3);
+        //data = findViewById(R.id.editTextTextPersonName3);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
