2400ae9b6c173ff304639be72fbc6efa660906b6
Πάτροκλος Γεωργίαδης <efthimisgeorgiadis17@gmail.com>
2023-01-31 15:31:53 +0200

    sign up process

--- app/src/main/java/com/example/smartalert/SignUp.java
+++ app/src/main/java/com/example/smartalert/SignUp.java
@@ -1,14 +1,48 @@
 package com.example.smartalert;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
 
+    EditText email,password,role;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        email= findViewById(R.id.editTextTextPersonName);
+        password=findViewById(R.id.editTextTextPersonName4);
+        //role = findViewById(R.id.editTextTextPersonName3);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void signup(View view){
+        mAuth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            showMessage("Success!","User authenticated");
+                            showMessage("Success!",mAuth.getUid());
+                        }else {
+                            showMessage("Error",task.getException().getLocalizedMessage());
+                        }
+                    }
+                });
+    }
+    void showMessage(String title, String message){
+        new AlertDialog.Builder(this).setTitle(title).setMessage(message).setCancelable(true).show();
     }
 }
