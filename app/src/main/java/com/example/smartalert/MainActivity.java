8b9f5ae07790b2b03ef16abbe71f689502923988
Πάτροκλος Γεωργίαδης <efthimisgeorgiadis17@gmail.com>
2023-01-31 14:56:44 +0200

    sign up + sign in (ui)

--- app/src/main/java/com/example/smartalert/MainActivity.java
+++ app/src/main/java/com/example/smartalert/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.smartalert;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -34,7 +35,10 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
-
+    public void go_sign_up(View view){
+        Intent intent = new Intent(MainActivity.this, SignUp.class);
+        startActivity(intent);
+    }
     void showMessage(String title, String message){
         new AlertDialog.Builder(this).setTitle(title).setMessage(message).setCancelable(true).show();
     }
