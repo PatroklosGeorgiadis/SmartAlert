83901014b9f2a608ac5d39851a3a0fcd1a3f5e27
Πάτροκλος Γεωργίαδης <efthimisgeorgiadis17@gmail.com>
2023-02-02 20:10:51 +0200

    pass values from one activity to the next one

--- app/src/main/java/com/example/smartalert/MainActivity.java
+++ app/src/main/java/com/example/smartalert/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -53,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
                                     }
                                     else{
                                         Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                                        intent.putExtra("ID", mAuth.getUid());
                                         startActivity(intent);
                                     }
                                 }
--- app/src/main/java/com/example/smartalert/UserActivity.java
+++ app/src/main/java/com/example/smartalert/UserActivity.java
@@ -2,6 +2,7 @@ package com.example.smartalert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,6 +12,9 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        /*Intent prev_intent = getIntent();
+        String str = prev_intent.getStringExtra("ID");*/
+
     }
 
     public void go_submission(View view) {
