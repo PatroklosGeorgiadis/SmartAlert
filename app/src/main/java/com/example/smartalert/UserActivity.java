8430d0e32269aee7b69b5e1994d17cd038e9e3f2
Πάτροκλος Γεωργίαδης <efthimisgeorgiadis17@gmail.com>
2023-02-02 22:55:07 +0200

    time for submission

--- app/src/main/java/com/example/smartalert/UserActivity.java
+++ app/src/main/java/com/example/smartalert/UserActivity.java
@@ -5,19 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class UserActivity extends AppCompatActivity {
 
+    String ID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-        /*Intent prev_intent = getIntent();
-        String str = prev_intent.getStringExtra("ID");*/
-
+        Intent prev_intent = getIntent();
+        ID = prev_intent.getStringExtra("ID");
     }
 
     public void go_submission(View view) {
-
+        Button btn = (Button) view;
+        String emergency = btn.getText().toString();
+        Intent intent = new Intent(UserActivity.this, Submission.class);
+        intent.putExtra("ID", ID);
+        intent.putExtra("Emergency", emergency);
+        startActivity(intent);
     }
 }
