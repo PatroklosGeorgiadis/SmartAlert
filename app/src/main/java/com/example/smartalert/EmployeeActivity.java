3e9b308a6469049298ec992bce31c2205c93e33f
Πάτροκλος Γεωργίαδης <efthimisgeorgiadis17@gmail.com>
2023-02-07 15:01:49 +0200

    small changes

--- app/src/main/java/com/example/smartalert/EmployeeActivity.java
+++ app/src/main/java/com/example/smartalert/EmployeeActivity.java
@@ -20,10 +20,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 
